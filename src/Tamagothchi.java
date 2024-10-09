a6901e1440d3f5f58f80a535470815e13d1912dd
Tia Hedlund <tia.hedlund@elev.ga.ntig.se>
2024-10-09 12:11:49 +0200

    fixat speak words metoden

--- src/Tamagothchi.java
+++ src/Tamagothchi.java
@@ -3,26 +3,38 @@ import java.util.Random;
 
 public class Tamagothchi {
 
-    private int hunger;
-    private int boredom;
-    private boolean isAlice;
+    private int hunger = 0;
+    private int boredom = 0;
+    private boolean isAlice = true;
     private ArrayList<String> words;
     private Random generator = new Random();
-    public String Name;
+    public String name;
 
+    // constructor
+    public Tamagothchi(String incomingName) {
+        name = incomingName;
+    }
 
     public void tick(){
         hunger += 1;
         boredom += 1;
+
     }
 
     public void feed(){
         hunger -= 1;
+        if (hunger < 0){
+            hunger = 1;
+        }
     }
 
     public void speak(){
-
-        System.out.println();
+        if (words.isEmpty()){
+            System.out.println("No words known...");
+        }
+        else{
+            System.out.println(words.get(generator.nextInt(words.size())));
+        }
         reduceBoredom();
     }
 
@@ -33,18 +45,21 @@ public class Tamagothchi {
     }
 
     public void printStats(){
+        System.out.println(name + "s Stats:");
         System.out.println("Hunger: " + hunger);
         System.out.println("Boredom: " + boredom);
 
     }
 
     public boolean getAlive(){
-        return false; // temporär
+        return isAlice;
     }
 
     private void reduceBoredom(){
-
+        boredom -= 1;
+        if (boredom<0){
+            boredom = 0;
+        }
     }
-
 }
 
